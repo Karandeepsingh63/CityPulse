ad97a2758dfe0830b35462591f7231ae0a2c56b1
jashan0703 <145106532+jashan0703@users.noreply.github.com>
2024-05-12 17:15:06 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,13 +1,23 @@
-let cardNumInput = 
-	document.querySelector('#cardNum') 
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("paymentForm").addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent the default form submission behavior
 
-cardNumInput.addEventListener('keyup', () => { 
-	let cNumber = cardNumInput.value 
-	cNumber = cNumber.replace(/\s/g, "") 
+        // Here you can perform any necessary form validation or submission to the server
 
-	if (Number(cNumber)) { 
-		cNumber = cNumber.match(/.{1,4}/g) 
-		cNumber = cNumber.join(" ") 
-		cardNumInput.value = cNumber 
-	} 
-})
+        // Assuming the order is successfully placed, show a popup
+        alert("Order placed successfully!");
+    });
+});
+
+let cardNumInput = document.querySelector('#cardNum');
+
+cardNumInput.addEventListener('keyup', () => {
+    let cNumber = cardNumInput.value;
+    cNumber = cNumber.replace(/\s/g, "");
+
+    if (Number(cNumber)) {
+        cNumber = cNumber.match(/.{1,4}/g);
+        cNumber = cNumber.join(" ");
+        cardNumInput.value = cNumber;
+    }
+});
